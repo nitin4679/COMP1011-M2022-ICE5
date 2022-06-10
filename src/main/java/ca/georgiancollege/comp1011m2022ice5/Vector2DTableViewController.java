11c73aed3e69d79970510183632f38d243ef1870
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-06-10 14:07:49 -0400

    modified TableView Scene to include Magnitude

--- src/main/java/ca/georgiancollege/comp1011m2022ice5/Vector2DTableViewController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice5/Vector2DTableViewController.java
@@ -25,6 +25,9 @@ public class Vector2DTableViewController implements Initializable
     @FXML
     private TableColumn<Vector2D, Float> YColumn;
 
+    @FXML
+    private TableColumn<Vector2D, Float> MagnitudeColumn;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
@@ -32,6 +35,7 @@ public class Vector2DTableViewController implements Initializable
        VectorIDColumn.setCellValueFactory(new PropertyValueFactory<>("ID"));
        XColumn.setCellValueFactory(new PropertyValueFactory<>("X"));
        YColumn.setCellValueFactory(new PropertyValueFactory<>("Y"));
+       MagnitudeColumn.setCellValueFactory(new PropertyValueFactory<>("Magnitude"));
 
        tableView.getItems().addAll(DBManager.Instance().readVectorTable());
 
