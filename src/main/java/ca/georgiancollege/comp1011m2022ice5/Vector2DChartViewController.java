1d377ef132321eddb954e5e5eded8498d4b519e3
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-06-10 14:36:29 -0400

    added UI for ChartView

--- src/main/java/ca/georgiancollege/comp1011m2022ice5/Vector2DChartViewController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice5/Vector2DChartViewController.java
@@ -1,4 +1,26 @@
 package ca.georgiancollege.comp1011m2022ice5;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
 public class Vector2DChartViewController {
+
+    @FXML
+    private BarChart<String, Float> barChart;
+
+    @FXML
+    private CategoryAxis categoryAxis;
+
+    @FXML
+    private NumberAxis numberAxis;
+
+    @FXML
+    private void LoadTableView(ActionEvent event)
+    {
+
+    }
+
 }
