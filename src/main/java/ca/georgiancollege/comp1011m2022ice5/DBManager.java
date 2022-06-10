7c092ccfe007e7da8ae68809add02a914d808626
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-06-10 12:08:05 -0400

    added project files for ICE5

---
+++ src/main/java/ca/georgiancollege/comp1011m2022ice5/DBManager.java
@@ -0,0 +1,124 @@
+package ca.georgiancollege.comp1011m2022ice5;
+
+import javafx.fxml.FXML;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+// singleton
+public class DBManager
+{    /********************** SINGLETON SECTION **************************/
+    //step 1- private static instance member variable
+    private static DBManager m_instance = null;
+    //step2 - make the default constructor private
+    private DBManager(){}
+    //step3 - create a public static entry point / instance method
+    public static DBManager Instance()
+    {
+        //step4 - check if the private instance member variable is null
+        if(m_instance == null)
+        {
+            //step5 - Instantiate a new DBManager instance
+            m_instance = new DBManager();
+        }
+        return m_instance;
+    }
+    /********************************************************************* */
+
+
+
+    // private instance member variables
+    private String m_user = "student";
+    private String m_password = "123456";
+    private String m_connectURL = "jdbc:mysql://localhost:3306/comp1011m2022";
+
+    /**
+     * This method inserts a Vector2D object to the Database
+     * @param vector2D
+     * @return
+     * @throws SQLException
+     */
+    public int insertVector2D(Vector2D vector2D) throws SQLException {
+        int vectorID = -1; // if this method returns -1 - it means that something went wrong
+        //initializing the result set object
+        ResultSet resultSet = null;
+
+        //create  a query string
+        String sql = "Insert into vectors(X,Y) values(?,?);";
+
+        try
+        ( /*head of the try / catch block */
+            Connection connection = DriverManager.getConnection(m_connectURL, m_user, m_password);
+            PreparedStatement statement = connection.prepareStatement(sql, new String[] {"vectorID"});
+
+        )
+        {
+
+            // configure prepared  statement
+            statement.setFloat(1,vector2D.getX());
+            statement.setFloat(2, vector2D.getY());
+
+            //run the command on the Database
+
+            statement.executeUpdate();
+
+            //get the vectorID
+            resultSet = statement.getGeneratedKeys();
+            while (resultSet.next())
+            {
+                //get the VectorID from the database
+                vectorID = resultSet.getInt(1);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if (resultSet != null)
+            {
+                resultSet.close();
+            }
+        }
+
+        return vectorID;
+    }
+
+    /**
+     * This method reads the vectors table from the MySQL database and returns an
+     * ArrayList of Vector2D type.
+     * @return
+     */
+    public ArrayList<Vector2D> readVectorTable()
+    {
+        // Instantiates an ArrayList collection of type Vector2D  (empty container)
+        ArrayList<Vector2D> vectors = new ArrayList<Vector2D>();
+
+        String sql = "select vectors.vectorID, X, Y from vectors group by vectors.vectorID";
+
+        try (Connection connection = DriverManager.getConnection(m_connectURL, m_user, m_password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);)
+        {
+            // while there is another record ..... loop
+            while (resultSet.next())
+            {
+                //decode data from the database table
+                int vectorID = resultSet.getInt("vectorID");
+                float X = resultSet.getFloat("X");
+                float Y = resultSet.getFloat("Y");
+
+                // create an anonymous Vector2D object with the data from the database
+                // then add the new Vector2D object to the vectors ArrayList
+                vectors.add(new Vector2D(vectorID, X, Y));
+
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+
+        return vectors;
+    }
+}
